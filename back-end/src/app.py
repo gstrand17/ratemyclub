e2f897610277dd79fc6b9e76c617618dacfb8294
TreeOfJuly <treeofjuly@gmail.com>
2024-10-16 13:44:42 -0400

    Added new API routes

--- back-end/src/app.py
+++ back-end/src/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, Float
+from flask import Flask
 import os
 
 
 app = Flask(__name__) #__name__ means name of app takes name from script
 
+from routes import *  # Import after app is initialized
+from models import *
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
