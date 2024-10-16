99918e282fc4f0a42b3a0b0c2316f50031569379
gstrand17 <gstrand@ufl.edu>
2024-10-16 13:55:38 -0400

    de

--- back-end/src/models.py
+++ back-end/src/models.py
@@ -1,3 +1,5 @@
+from multiprocessing import Array
+
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float
@@ -9,7 +11,50 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join('C:/Users/gr
 
 db = SQLAlchemy(app) # initializes database with a constructor
 
+# ORM's work by taking objects/classes and converting them to SQL behind the scenes
+#database models
+class User(db.Model): # there will be one instance of this class
+    __tablename__ = 'users'
+    email = Column(String, primary_key=True, unique=True)
+    username = Column(String, unique=True)
+    password = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    student = Column(Boolean)
+    admin = Column(Boolean)
+    club_exec = Column(Boolean)
+    clubs = Column(String) #for club exec
+    passkey = Column(Integer) #for admin
 
+
+class ClubDirectory(db.Model): # there will be one instance of this class
+    __tablename__ = 'club_directory'
+    ID = Column(Integer, primary_key=True) #this needs to be the hash value
+    club_name = Column(String)
+    tags = Column(Array(String))
+    avg_overall_rating = Column(Float)
+    avg_soc_rating = Column(Float)
+    avg_acad_rating = Column(Float)
+    avg_exec_rating = Column(Float)
+    avg_comlev = Column(Float)
+    active_mem = Column(Integer)
+    description = Column(String)
+    link = Column(String)
+
+
+class ClubReviews(db.Model): # there will be lots of instances of this class
+     # somehow the name of an instance needs to correspond with the hashed ID
+    __tablename__ = 'club_reviews'
+    user = Column(String, primary_key=True)
+    date = Column(String) # date format?
+    overall_rating = Column(Float)
+    soc_rating = Column(Float)
+    acad_rating = Column(Float)
+    exec_rating = Column(Float)
+    comlev = Column(Float)
+    current_mem = Column(Boolean)
+    time_mem = Column(String) # date format?
+    paid = Column(Boolean)
 
 @app.cli.command('db_create') #command line word to do something
 def db_create():
@@ -33,7 +78,7 @@ def db_seed():
 
 # ORM's work by taking objects/classes and converting them to SQL behind the scenes
 #database models
-class User(db.Model):
+class User(db.Model): # there will be one instance of this class
     __tablename__ = 'users'
     email = Column(String, primary_key=True, unique=True)
     username = Column(String, unique=True)
@@ -45,4 +90,34 @@ class User(db.Model):
     club_exec = Column(Boolean)
     clubs = Column(String) #for club exec
     passkey = Column(Integer) #for admin
+
+
+class ClubDirectory(db.Model): # there will be one instance of this class
+    __tablename__ = 'club_directory'
+    ID = Column(Integer, primary_key=True) #this needs to be the hash value
+    club_name = Column(String)
+    tags = Column(Array(String))
+    avg_overall_rating = Column(Float)
+    avg_soc_rating = Column(Float)
+    avg_acad_rating = Column(Float)
+    avg_exec_rating = Column(Float)
+    avg_comlev = Column(Float)
+    active_mem = Column(Integer)
+    description = Column(String)
+    link = Column(String)
+
+
+class ClubReviews(db.Model): # there will be lots of instances of this class
+     # somehow the name of an instance needs to correspond with the hashed ID
+    __tablename__ = 'club_reviews'
+    user = Column(String, primary_key=True)
+    date = Column(String) # date format?
+    overall_rating = Column(Float)
+    soc_rating = Column(Float)
+    acad_rating = Column(Float)
+    exec_rating = Column(Float)
+    comlev = Column(Float)
+    current_mem = Column(Boolean)
+    time_mem = Column(String) # date format?
+    paid = Column(Boolean)
 
