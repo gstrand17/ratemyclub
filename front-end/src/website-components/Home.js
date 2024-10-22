556859f6598bbbda41731cf194c75211b831e622
erink <Ehargrave@ufl.edu>
2024-10-22 13:20:19 -0400

    Enhanced home page

--- front-end/src/website-components/Home.js
+++ front-end/src/website-components/Home.js
@@ -3,18 +3,31 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
-        <div style={{textAlign: 'center', marginTop: '50px'}}>
+        <div style={{
+            textAlign: 'center',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            height: '100vh',
+            //backgroundImage: 'url(/uf_image.png)',
+            backgroundImage: 'url(/uf.png)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+        }}>
             <h1 style={{
                 fontFamily: "'Alfa Slab One', serif",
-                fontSize: '4rem',
+                fontSize: '5rem',
             }}>
                 Welcome to RateMyClub
             </h1>
-            <p style={{fontSize: '1.2rem', fontFamily: "'Alfa Slab One', serif", color: '#555'}}>
+
+            <p style={{fontSize: '1.5rem', fontFamily: "'Alfa Slab One', serif", color: '#1a1a1a'}}>
                 Your one-stop destination to rate and review clubs at the University of Florida.
             </p>
             <Link to="/login">
-                <button style={{padding: '10px 20px', fontSize: '16px', backgroundColor: '#59a7ff',}}>Login to Get Started</button>
+                <button style={{padding: '10px 20px', fontSize: '16px', backgroundColor: '#59a7ff',}}>Login to Get
+                    Started
+                </button>
             </Link>
         </div>
     );
