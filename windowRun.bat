c84a2e8e95cd5adada3357dafc17e236d5a4b043
Julio Cesar Arboleda <166689388+TreeOfJuly@users.noreply.github.com>
2025-01-06 13:24:13 -0500

    Update windowRun.bat

--- windowRun.bat
+++ windowRun.bat
@@ -1,21 +1,12 @@
 @echo off
 
-REM Function to start Vite frontend and Flask backend
-call :start_project
-
-goto :eof
-
-:start_project
 echo Starting Vite frontend...
 cd front-end
-REM Ensure dependencies are installed and start Vite
-npm install REM Install dependencies if necessary
-start npm run dev REM Run Vite server in a new window
+npm install 
+npm run dev 
 
 echo Starting Flask backend...
 cd ..\back-end
-REM Activate the virtual environment (Windows-specific)
-call venv\Scripts\activate.bat
+venv\Scripts\activate
 cd src
 python app.py
-goto :eof
